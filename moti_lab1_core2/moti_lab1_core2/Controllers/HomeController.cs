a4a5da1a7ff31535d05815539db2c90e970bf497
Олександр Тунік <kinonadzami98@gmail.com>
2017-10-06 12:45:28 +0300

    EditScrips

--- moti_lab1_core2/moti_lab1_core2/Controllers/HomeController.cs
+++ moti_lab1_core2/moti_lab1_core2/Controllers/HomeController.cs
@@ -169,15 +169,15 @@ namespace moti_lab1_core2.Controllers
         public IActionResult EditAlternative(int ANum)
         {
             Alternative alternative = db.Alternatives.First(x => x.ANum == ANum);
-            return View();
+            return View(alternative);
         }
 
         [HttpPost]
         public IActionResult UpdateAlternative(int ANum, string alternativeName)
         {
-            Alternative newAlt =db.Alternatives.First(x => x.ANum == ANum);
-            newAlt.AName = alternativeName;
-            db.Alternatives.Update(newAlt);
+            Alternative Alt =db.Alternatives.First(x => x.ANum == ANum);
+            Alt.AName = alternativeName;
+            db.Alternatives.Update(Alt);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
@@ -185,17 +185,26 @@ namespace moti_lab1_core2.Controllers
         [HttpPost]
         public IActionResult EditCriterion(int CNum)
         {
+            Criterion criterion = db.Criterions.First(x => x.CNum == CNum);
             List<SelectListItem> variants = new List<SelectListItem>
         {
             new SelectListItem { Value = "quality", Text = "Качественный" },
             new SelectListItem { Value = "quantity", Text = "Количественный" }
         };
+            if (criterion.CType == "quality")
+            {
+                variants.First(x => x.Value == "quality").Selected = true;
+            }
             ViewBag.Type = variants;
             List<SelectListItem> optims = new List<SelectListItem>
         {
             new SelectListItem { Value = "min", Text = "Минимум" },
             new SelectListItem { Value = "max", Text = "Максимум" }
         };
+            if (criterion.OptimType == "min")
+            {
+                optims.First(x => x.Value == "min").Selected = true;
+            }
             ViewBag.Optim = optims;
             List<SelectListItem> scales = new List<SelectListItem>
         {
@@ -203,12 +212,26 @@ namespace moti_lab1_core2.Controllers
             new SelectListItem { Value = "point", Text = "Бальная" },
             new SelectListItem { Value = "range", Text = "Ранговая" }
         };
+            switch (criterion.ScaleType)
+            {
+                case "num":
+                    scales.First(x => x.Value == "num").Selected = true;
+                    break;
+                case "point":
+                    scales.First(x => x.Value == "point").Selected = true;
+                    break;
+                case "range":
+                    scales.First(x => x.Value == "range").Selected = true;
+                    break;
+                default:
+                    break;
+            }
             ViewBag.Scales = scales;
-            return View();
+            return View(criterion);
         }
 
         [HttpPost]
-        public IActionResult EditCriterion(int CNum, string CName, string CType, string OptimType, string EdIzmer, string ScaleType)
+        public IActionResult UpdateCriterion(int CNum, string CName, string CType, string OptimType, string EdIzmer, string ScaleType)
         {
             Criterion newCriterion = new Criterion();
             newCriterion.CName = CName;
@@ -216,7 +239,7 @@ namespace moti_lab1_core2.Controllers
             newCriterion.OptimType = OptimType;
             newCriterion.Edizmer = EdIzmer;
             newCriterion.ScaleType = ScaleType;
-            db.Criterions.Add(newCriterion);
+            db.Criterions.Update(newCriterion);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
@@ -224,23 +247,32 @@ namespace moti_lab1_core2.Controllers
         [HttpPost]
         public IActionResult EditMark(int MNum)
         {
+            Mark mark = db.Marks.First(x => x.MNum == MNum);
             List<Criterion> list = db.Criterions.ToList();
             List<SelectListItem> selects = new List<SelectListItem>();
             foreach (Criterion item in list)
             {
                 selects.Add(new SelectListItem { Value = "" + item.CNum, Text = item.CName });
+                if (item.CNum == mark.CNum)
+                {
+                    selects.Add(new SelectListItem { Value = "" + item.CNum, Text = item.CName, Selected = true });
+                }
+                else
+                {
+                    selects.Add(new SelectListItem { Value = "" + item.CNum, Text = item.CName });
+                }
             }
             ViewBag.Select = selects;
-            return View();
+            return View(mark);
         }
 
         [HttpPost]
-        public IActionResult EditMark(int MNum, string MName, int CNum)
+        public IActionResult UpdateMark(int MNum, string MName, int CNum)
         {
-            Mark newMark = new Mark();
-            newMark.MNAme = MName;
-            newMark.CNum = CNum;
-            db.Marks.Add(newMark);
+            Mark mark = db.Marks.First(x => x.MNum == MNum);
+            mark.MNAme = MName;
+            mark.CNum = CNum;
+            db.Marks.Add(mark);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
@@ -248,28 +280,44 @@ namespace moti_lab1_core2.Controllers
         [HttpPost]
         public IActionResult EditVector(int VNum)
         {
+            Vector vector = db.Vectors.First(x => x.VNum == VNum);
             List<SelectListItem> alts = new List<SelectListItem>();
             foreach (Alternative item in db.Alternatives.ToList())
             {
-                alts.Add(new SelectListItem { Value = "" + item.ANum, Text = item.AName });
+                if (item.ANum == vector.Anum)
+                {
+                    alts.Add(new SelectListItem { Value = "" + item.ANum, Text = item.AName, Selected = true });
+                }
+                else
+                {
+                    alts.Add(new SelectListItem { Value = "" + item.ANum, Text = item.AName });
+                }
             }
             List<SelectListItem> marks = new List<SelectListItem>();
             foreach (Mark item in db.Marks.ToList())
             {
                 marks.Add(new SelectListItem { Value = "" + item.MNum, Text = item.MNAme });
+                if (item.MNum == vector.MNum)
+                {
+                    marks.Add(new SelectListItem { Value = "" + item.MNum, Text = item.MNAme, Selected = true});
+                }
+                else
+                {
+                    marks.Add(new SelectListItem { Value = "" + item.MNum, Text = item.MNAme });
+                }
             }
             ViewBag.alts = alts;
             ViewBag.marks = marks;
-            return View();
+            return View(vector);
         }
 
         [HttpPost]
-        public IActionResult EditVector(int VNum, int ANum, int MNum)
+        public IActionResult UpdateVector(int VNum, int ANum, int MNum)
         {
-            Vector newVector = new Vector();
-            newVector.Anum = ANum;
-            newVector.MNum = MNum;
-            db.Vectors.Add(newVector);
+            Vector Vector = db.Vectors.First(x => x.VNum == VNum);
+            Vector.Anum = ANum;
+            Vector.MNum = MNum;
+            db.Vectors.Update(Vector);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
